195b7036b98b7539e87a19ed2e679ba044f4e170
Davidokt <85350280+Dav1dushka@users.noreply.github.com>
2026-06-17 01:16:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,27 +9,48 @@ function calculateNetwork() {
     if (!ipRegex.test(ip)) {
 
         document.getElementById("result").innerHTML = `
-<div class="result-box">
+            <div class="error">
+                Invalid IPv4 Address
+            </div>
+        `;
 
-    <div class="result-row">
-        <span class="label">Network Address</span>
-        <span class="value">${network}</span>
-    </div>
+        return;
+    }
 
-    <div class="result-row">
-        <span class="label">Broadcast Address</span>
-        <span class="value">${broadcast}</span>
-    </div>
+    const parts = ip.split(".");
 
-    <div class="result-row">
-        <span class="label">CIDR</span>
-        <span class="value">/${cidr}</span>
-    </div>
+    const network =
+        `${parts[0]}.${parts[1]}.${parts[2]}.0`;
 
-    <div class="result-row">
-        <span class="label">Usable Hosts</span>
-        <span class="value">${hosts}</span>
-    </div>
+    const broadcast =
+        `${parts[0]}.${parts[1]}.${parts[2]}.255`;
 
-</div>
-`;
+    const hosts =
+        Math.pow(2, 32 - cidr) - 2;
+
+    document.getElementById("result").innerHTML = `
+        <div class="result-box">
+
+            <div class="result-row">
+                <span class="label">Network Address</span>
+                <span class="value">${network}</span>
+            </div>
+
+            <div class="result-row">
+                <span class="label">Broadcast Address</span>
+                <span class="value">${broadcast}</span>
+            </div>
+
+            <div class="result-row">
+                <span class="label">CIDR Notation</span>
+                <span class="value">/${cidr}</span>
+            </div>
+
+            <div class="result-row">
+                <span class="label">Usable Hosts</span>
+                <span class="value">${hosts}</span>
+            </div>
+
+        </div>
+    `;
+}
